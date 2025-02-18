dc42f5d282793779377e82f814285dd54c8159eb
Alexandre <71261076+AlexandreSLangunno@users.noreply.github.com>
2025-02-17 21:39:58 -0300

    Create configs template file

---
+++ configs.py
@@ -0,0 +1,25 @@
+export_cookies = [
+  {
+    "domain": "streamyard.com",
+    "expirationDate": 1742338830.35298,
+    "hostOnly": True,
+    "httpOnly": False,
+    "name": "language",
+    "path": "/",
+    "sameSite": "unspecified",
+    "secure": True,
+    "session": False,
+    "storeId": "0",
+    "value": "pt",
+    "origin": "https://streamyard.com"
+  }
+    # ... Outher 
+]
+
+channel_name = "Channel Name" 
+
+titles = [
+    "Title 1",
+    "Title 2" 
+    # ...
+]
